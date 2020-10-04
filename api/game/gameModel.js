a2dad33e57e99d9ba836a0b047b90ac6fe2e48bc
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-04 14:18:16 -0700

    Added GET endpoint to view a user's instances.

--- api/game/gameModel.js
+++ api/game/gameModel.js
@@ -4,6 +4,13 @@ const newInstance = (info) => {
   return db('Games').insert(info).returning(['ID']);
 };
 
+const getInstancesByUser = (OwnerID) => {
+  return db('Games')
+    .where({ OwnerID })
+    .select(['ID', 'Title', 'Finished', 'WinningTeam AS Winner']);
+};
+
 module.exports = {
   newInstance,
+  getInstancesByUser,
 };
--- api/game/gameRouter.js
+++ api/game/gameRouter.js
@@ -4,6 +4,11 @@ const Games = require('./gameModel');
 const { authRequired } = require('../middleware/');
 const { ops } = require('../../lib');
 
+router.get('/', authRequired, (req, res) => {
+  const { ID } = req.decodedToken;
+  ops.getAll(res, Games.getInstancesByUser, 'Game', ID);
+});
+
 router.post('/', authRequired, (req, res) => {
   const { ID } = req.decodedToken;
   const instance = req.body;
